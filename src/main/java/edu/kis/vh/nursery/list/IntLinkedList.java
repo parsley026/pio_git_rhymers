a41b301201ca2bb347a361fdd168b1a0ec4e6d98
Sara Tkaczyk <114811833+fxlipton@users.noreply.github.com>
2024-05-12 19:30:50 +0200

    Zmiana literalu na stala EMPTY w pliku IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,7 +1,7 @@
 package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
-
+    public static final EMPTY = -1;
     Node last;
     int i;
 
@@ -25,13 +25,13 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
+            return EMPTY;
         return last.value;
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
+            return EMPTY;
         final int ret = last.value;
         last = last.prev;
         return ret;
