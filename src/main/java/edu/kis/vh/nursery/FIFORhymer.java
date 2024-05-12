ff7ae96f330361121c3f8eb9a5a62be5f11de828
parsley026 <245828@edu.p.lodz.pl>
2024-05-12 20:02:40 +0200

    9. zmiana widocznosci atrybutow klas na mozliwie najmniejsza

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,10 +2,10 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
     @Override
-    public int countOut() {
+    protected int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
 
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,11 +4,12 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     int totalRejected = 0;
 
-    public int reportRejected() {
+    protected int reportRejected() {
         return totalRejected;
     }
+
     @Override
-    public void countIn(int in) {
+    protected void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
         else
