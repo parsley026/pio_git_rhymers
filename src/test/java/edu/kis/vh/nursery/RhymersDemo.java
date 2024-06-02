769cc2f589e9cef2f8e4abf3155f4ed8397fd34b
parsley026 <245828@edu.p.lodz.pl>
2024-06-02 14:49:40 +0200

    13. uporzadkowanie aplikacji demo RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -8,29 +8,31 @@ class RhymersDemo {
     public static final int ARRAY_SIZE = 15;
     public static final int LAST_INDEX = 3;
     public static final int MAX_RANGE = 20;
+
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(Rhymersfactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
+
         for (int i = 1; i < ARRAY_SIZE; i++)
             for (int j = 0; j < LAST_INDEX; j++)
                 rhymers[j].countIn(i);
-        
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < ARRAY_SIZE; i++)
             rhymers[LAST_INDEX].countIn(rn.nextInt(MAX_RANGE));
-        
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[LAST_INDEX]).reportRejected());
-        
     }
-    
 }
